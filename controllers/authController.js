06f4eee5fa22b0e18892afcb04a6320661b8abc1
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-08 09:16:54 +0200

    feat: register logic done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,17 @@
 const userModel = require('../models/userModel');
+
+const registerGET = (req, res) => {
+  res.render('register', { title: 'Register' });
+};
+
+const registerPOST = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await userModel.registerUser(name, email, password);
+    res.redirect('/auth/login');
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+module.exports = { registerGET, registerPOST };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-
+const authController = require('../controllers/authController');
 const authRouter = Router();
 
 authRouter.get('/login');
 authRouter.post('/login');
-authRouter.get('/register');
-authRouter.post('/register');
+authRouter.get('/register', authController.registerGET);
+authRouter.post('/register', authController.registerPOST);
 authRouter.get('/logout');
 
 module.exports = authRouter;
