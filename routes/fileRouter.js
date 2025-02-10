54dc5a9de814964f55f467f5aab8fa2865cafdbe
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-11 00:33:12 +0200

    feat: add size limit for uploads

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/', limits: { fileSize: 1000000 } });
 const fileController = require('../controllers/fileController');
 
 const fileRouter = Router();
